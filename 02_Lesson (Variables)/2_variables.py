2d87b016487475798c10e5eaf6cacc730fd4b425
Muhammad Kamran Khan <109745004+Kamran-it009@users.noreply.github.com>
2025-01-21 19:17:34 +0500

    Update 2_variables.py

--- 02_Lesson (Variables)/2_variables.py
+++ 02_Lesson (Variables)/2_variables.py
@@ -2,6 +2,7 @@
 # Text (String)
 # Decimal numbers (float)
 # Without Decimal numbers (integer)
+# True or False (Boolean)
 # Complex Numbers (comp)
 # List
 # Tuples
@@ -11,6 +12,7 @@ name = 'KAMRAN' # string variable
 age = 15 # integer
 height = 5.4 # float
 comp = 20 + 2j
+martial_status = True
 print('My name is:', name)
 print('My age is:', age)
 print('My height is:', height)
